febf45f9e3baa06ca57d607064c9d6e551307d26
w3soto <soto.ms@gmail.com>
2022-07-16 09:49:27 +0200

    Add support for built in themes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
 
 export interface Theme {
   name: string;
@@ -15,15 +15,25 @@ export interface Theme {
 export class AppComponent implements OnInit {
 
   themes: Theme[] = [
-    {name: 'Hireme', className: 'hireme-theme', source: 'hireme-theme.css'},
     {name: 'Arc light', className: 'arc-light-theme', source: 'arc-theme.css'},
-    {name: 'Arc dark', className: 'arc-dark-theme', source: 'arc-theme.css'}
+    {name: 'Arc dark', className: 'arc-dark-theme', source: 'arc-theme.css'},
+    {name: 'Hireme', className: 'hireme-theme', source: 'hireme-theme.css'},
+    {name: 'Deep Purple & Amber (built in)', className: 'deeppurple-amber', source: './assets/prebuilt-themes/deeppurple-amber.css'},
+    {name: 'Indigo & Pink (built in)', className: 'indigo-pink', source: './assets/prebuilt-themes/indigo-pink.css'},
+    {name: 'Pink & Blue-grey (built-in)', className: 'pink-bluegrey', source: './assets/prebuilt-themes/pink-bluegrey.css'},
+    {name: 'Purple & Green (built-in)', className: 'purple-green', source: './assets/prebuilt-themes/purple-green.css'},
   ];
 
   theme?: Theme;
 
   leftMenuOpened: boolean = true;
   rightMenuOpened: boolean = false;
+
+  constructor(
+    private _ngZone: NgZone
+  ) {
+
+  }
 
   ngOnInit() {
     let theme = null;
@@ -38,37 +48,54 @@ export class AppComponent implements OnInit {
   }
 
   loadTheme(source: string): Promise<void> {
-    let found = document.head.querySelector('link[rel="stylesheet"][href="' + source + '"]');
-    if (!found) {
-      return new Promise((resolve, reject) => {
-        const link = document.createElement('link');
-        link.rel = 'stylesheet';
-        link.href = source;
-        link.onload = () => {
-          console.log('XXX LOAD');
-          resolve();
-        };
-        link.onerror = () => reject();
-        document.head.appendChild(link);
-      });
-    }
-    else {
-      return new Promise((resolve, reject) => {
-        console.log('XXX LOAD 2 ');
-        resolve()
-      });
-    }
+    let link: HTMLLinkElement = document.getElementById('theme') as HTMLLinkElement;
+    link?.remove();
+    return new Promise((resolve, reject) => {
+      const link = document.createElement('link');
+      link.rel = 'stylesheet';
+      link.id = "theme";
+      link.href = source;
+      link.onload = () => {
+        resolve();
+      };
+      link.onerror = () => {
+        reject();
+      };
+      document.head.appendChild(link);
+    });
+
   }
 
   selectTheme(theme: Theme) {
-    this.loadTheme(theme.source).then(() => {
-      this.themes.forEach(t => {
-        document.body.classList.remove(t.className);
+
+    const transition = 200;
+
+    document.body.style.transition = "opacity " + transition + "ms";
+    document.body.style.opacity = "0";
+
+      setTimeout(() => {
+
+      this.loadTheme(theme.source).then(() => {
+        this.themes.forEach(t => {
+          document.body.classList.remove(t.className);
+        });
+        document.body.classList.add(theme.className);
+        this.theme = theme;
+        window.localStorage.setItem('theme', theme.className);
+
+        setTimeout(() => {
+          document.body.style.opacity = "1";
+        }, 1);
+
+      }).catch(() => {
+
+        setTimeout(() => {
+          document.body.style.opacity = "1";
+        }, 1);
+
       });
-      document.body.classList.add(theme.className);
-      this.theme = theme;
-      window.localStorage.setItem('theme', theme.className);
-    });
+
+    }, transition + 1);
   }
 
   toggleLeftMenu() {
