2c957aae9707491994bcd43caad6f9f7eeb8663b
w3soto <soto.ms@gmail.com>
2022-07-16 08:52:05 +0200

    Add selection list, add disabled items

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -7,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
+  items = [
+    {name: 'Pepper', disabled: false},
+    {name: 'Salt', disabled: false},
+    {name: 'Paprika', disabled: false},
+    {name: 'Tomato', disabled: true}
+  ];
+
+  actions = [
+    {name: 'Open', icon: 'upload_file', disabled: false},
+    {name: 'Save', icon: 'save', disabled: false},
+    {name: 'Undo', icon: 'undo', disabled: false},
+    {name: 'Delete', icon: 'delete', disabled: true},
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
